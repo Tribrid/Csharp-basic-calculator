79d83d09b1d008adf23bd757e4fefdcb3c8c1077
Nii-Quaye <niiquaye.ashie.7@gmail.com>
2023-01-29 01:16:34 +0000

    A basic Csharp calculator
    This is a basic C# calculator that performs addition, subtraction, multiplication and division of two numbers.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        double a, b;
+        char op;
+        Console.WriteLine("Enter first number");
+        a = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Enter second number");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Note: Enter operators between +, -, *, /");
+        Console.Write("Enter operator\n");
+        op = Convert.ToChar(Console.ReadLine());
+
+        if (op == '+')
+        {
+            Console.WriteLine($"Results of {a} + {b} = {a+b}");
+        }
+        else if (op == '-')
+        {
+            Console.WriteLine($"Results of {a} - {b} = {a-b}");
+        }
+        else if (op == '*')
+        {
+            Console.WriteLine($"Results of {a} * {b} = {a*b}");
+        }
+        else if (op == '/')
+        {
+            Console.WriteLine($"Results of {a} / {b} = {a/b}");
+        }
+        else
+        {
+            Console.WriteLine("Wrong operator");
+        }
+    }
+}
